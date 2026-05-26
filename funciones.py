9ffc8687d0fe4be94b8dd445c4d81ebae10c05ee
D4nte-11 <peruzzifacundo@gmail.com>
2026-05-26 18:44:28 -0300

    Initial commit

---
+++ funciones.py
@@ -0,0 +1,70 @@
+
+def cargar_paises():
+    paises = {}
+    try:
+        with open("paises.csv", "r") as archivo:
+            next(archivo)
+        
+        for linea in archivo:
+            datos = linea.strip().split(",")
+            nombre = datos[0]
+            continente = datos[1]
+            poblacion = int(datos[2])
+            superficie = float(datos[3])
+        paises[nombre] = {"continente": continente, "poblacion": poblacion, "superficie": superficie}
+    except FileNotFoundError:
+        print("Error: Archivo no encontrado.")
+        pass
+    return paises
+
+def guardar_paises(paises):
+    with open("paises.csv", "w") as archivo:
+        archivo.write("nombre,continente,poblacion,superficie\n")
+        for nombre, info in paises.items():
+            archivo.write(f"{nombre},{info['contienente']},{info['población']},{info['superficie']}\n")
+
+def agregar_pais(paises, continentes):
+    try:    
+        while True:
+            nombre = input("Introduzca el nombre del país: ").strip().title()
+            if nombre.replace(" ", "").isalpha():
+                if nombre not in paises:
+                    break
+                else: print("Error: El país ya se encuentra cargado.")
+            else: print("Error: El país introducido no es válido.")
+        while True:
+            continente = input("Ingrese el continente: ").strip().title()
+            if continente.isalpha():
+                if continente in continentes:
+                    break
+                else: print(f"Error: El continente introducido no existe, los contintentes existentes son: {continentes}")
+            else: print("Error: El continente introducudo no es válido.")
+        while True:
+            try:
+                poblacion = int(input(f"Ingrese la población de {nombre}: ").strip().replace(",","").replace(".",""))
+                if poblacion >= 0:
+                    break
+                else: print("Error: El valor ingresado debe ser mayor a 0.")
+            except ValueError:
+                print("Error: El valor ingresado no es un número.")
+        while True:
+            try:
+                superficie = float(input(f"Ingrese la superficice de {nombre}: ").strip())
+                if superficie > 0:
+                    break
+                else: print("Error: El valor ingresado debe ser mayor a 0.")
+            except ValueError:
+                print("Error: El valor ingresado no es un número.")
+    except Exception as e:
+        print("Ha ocurrido un error inesperado: ", type(e).__name__)
+    else:
+        paises[nombre] = {'continente':continente,'poblacion':poblacion,'superficie':superficie}
+        return f'País {nombre} ha sido agregado correctamente'
+
+# Create acá abajo las funciones para el 3 5 y 6
+
+def buscar_pais(paises):
+    print('Buscar paises')
+    buscar = input('Ingrese el pais que quiere buscar: ').strip()
+    if buscar in paises:
+        pass
---
+++ main.py
@@ -0,0 +1,37 @@
+from funciones import *
+paises = cargar_paises()
+
+print('''
+================
+|| Bienvenido ||
+================''')
+while True:
+    opcion = input('''
+1. Agregar un país nuevo
+2. Actualizar población y Superficie de un país
+3. Buscar país por Nombre
+4. Filtrar países
+5. Ordenar países
+6. Mostrar estadísticas
+7. Salir
+Introduzca una opción: ''')
+
+    match opcion:
+        case "1":
+            print(agregar_pais(paises,  continentes=['Europa', 'America', 'Asia', 'Africa', 'Oceania']))
+            guardar_paises(paises)
+        case "2":
+            pass
+        case "3":
+            buscar_pais(paises)
+        case "4":
+            pass
+        case "5":
+            pass
+        case "6":
+            pass
+        case "7":
+            print("Gracias por usar nuestro programa. Hasta luego!")
+            break
+        case _:
+            print("Error: la opción ingresada no es válida.")
